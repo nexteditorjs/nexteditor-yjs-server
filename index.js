2359f1ff9d089887c923abff622045671df05be8
nexteditorjs <nexteditorjs@gmail.com>
2022-03-12 22:44:24 +0800

    refactor: template

--- index.js
+++ index.js
@@ -1,36 +1,27 @@
-const Y = require('yjs')
 const { genId } = require('@nexteditorjs/nexteditor-core/dist/common');
 
 const setDocTemplate = require('./src/utils').setDocTemplate;
 //
 
-function getDefaultTemplate() {
-  //
-  // create empty doc
-  const titleBlockData = new Y.Map(Object.entries({
-    id: genId(),
-    type: 'text',
-    text: new Y.Text(),
-    'style-heading': 1,
-  }));
-  const contentBlockData = new Y.Map(Object.entries({
-    id: genId(),
-    type: 'text',
-    text: new Y.Text(),
-  }));
-  //
-  const blocks = new Y.Array();
-  blocks.push([titleBlockData, contentBlockData]);
-  //
-  const allBlocksObject = new Y.Map();
-  allBlocksObject.set('root', blocks);
-  //
-  return {
-    blocks: allBlocksObject,
-    meta: new Y.Map(),
-  };
-}
+const template = {
+  meta: {},
+  blocks: {
+    root: [
+      {
+        id: genId(),
+        type: 'text',
+        text: [],
+        'style-heading': 1,
+      },
+      {
+        id: genId(),
+        type: 'text',
+        text: [],
+      },
+    ],
+  },
+};
 
-setDocTemplate(getDefaultTemplate());
+setDocTemplate(template);
 //
 require('./src/server');
